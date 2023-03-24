1ab9b87cc0ddd4cb4c55b4674361f535bc5e6c08
santoshpatel4157 <asmisthecat@gmail.com>
2023-03-24 11:33:23 +0530

    react pack

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from "react"; 
+import ReactDOM from "react-dom/client"
+
+
+const head = React.createElement("h1",{id:"head"},"my app");
+
+        const head1 = ReactDOM.createRoot(document.getElementById("root"));
+
+        head1.render(head);
